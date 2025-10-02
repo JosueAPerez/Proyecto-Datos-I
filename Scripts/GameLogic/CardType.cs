01ea8f20d0596c4b8ff1693057258aa747ff0193
Marcelo-Matey-R <queridozapato@gmail.com>
2025-10-01 23:25:31 -0600

    Update CardType.cs

--- Scripts/GameLogic/CardType.cs
+++ Scripts/GameLogic/CardType.cs
@@ -1,20 +1,15 @@
 using Unity.Netcode;
 using Unity.Collections;
-
-public enum CardType
-{
-    Infanteria = 0,
-    Caballeria = 1,
-    Artilleria = 2
-}
+//permite facvorecer la serializacion de las cartas
 
 public struct CardSelection : INetworkSerializable
 {
-    public int idx0, idx1, idx2;
-    public int tipo0, tipo1, tipo2; // serializamos enums como int
+    public int idx0, idx1, idx2; //los indices del enum CardType
+    public int tipo0, tipo1, tipo2; //Los tipos de cartas que hay
 
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
+        //serializa todos los valores de los indices y tipos, para una mayor de facilidad de procesamiento
         serializer.SerializeValue(ref idx0);
         serializer.SerializeValue(ref idx1);
         serializer.SerializeValue(ref idx2);
@@ -40,26 +35,19 @@ public struct RemovedCardsInfo : INetworkSerializable
         serializer.SerializeValue(ref tipo1);
         serializer.SerializeValue(ref tipo2);
     }
+    //metodos para obtener el indice y el tipo de cartas removidas
+    public int GetIdxAt(int i) => i == 0 ? idx0 : i == 1 ? idx1 : i == 2 ? idx2 : -1;
+    public CardType GetTipoAt(int i) => (CardType)(i == 0 ? tipo0 : i == 1 ? tipo1 : tipo2);
+}
 
-    public int GetIdxAt(int i)
-    {
-        switch (i)
-        {
-            case 0: return idx0;
-            case 1: return idx1;
-            case 2: return idx2;
-            default: return -1;
-        }
-    }
+public struct CardData : INetworkSerializable
+{
+    public int territoryIdx;
+    public int tipo;
 
-    public CardType GetTipoAt(int i)
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
     {
-        switch (i)
-        {
-            case 0: return (CardType)tipo0;
-            case 1: return (CardType)tipo1;
-            case 2: return (CardType)tipo2;
-            default: return CardType.Infanteria;
-        }
+        serializer.SerializeValue(ref territoryIdx);
+        serializer.SerializeValue(ref tipo);
     }
 }
